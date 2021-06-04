29fad4898525761196d4306403cac949931f4c8d
medivb <69627753+medivb@users.noreply.github.com>
2021-06-04 20:03:10 +0200

    Removed other particles, added noise on field 5.

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 ESP8266_IoT.connectWifi("Broeknet", "B1vidhd10idl")
-OLED.init(128, 64)
 if (ESP8266_IoT.wifiState(true)) {
     basic.showString("Online")
 }
+OLED.init(128, 64)
 basic.forever(function () {
     ESP8266_IoT.connectThingSpeak()
     ESP8266_IoT.setData(
@@ -12,7 +12,7 @@ basic.forever(function () {
     Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C),
     Environment.octopus_BME280(Environment.BME280_state.BME280_humidity),
     Environment.octopus_BME280(Environment.BME280_state.BME280_pressure),
-    0,
+    Environment.ReadNoise(AnalogPin.P2),
     0
     )
     ESP8266_IoT.uploadData()
@@ -20,11 +20,8 @@ basic.forever(function () {
     OLED.writeString("Dust (ug/m3):")
     OLED.writeNum(Environment.ReadDust(DigitalPin.P13, AnalogPin.P1))
     OLED.newLine()
-    OLED.writeString("pm10 (ug/m3):")
-    OLED.writeNum(0)
-    OLED.newLine()
-    OLED.writeString("pm2.5 (ug/m3):")
-    OLED.writeNum(0)
+    OLED.writeString("Noise (dB)")
+    OLED.writeNum(Environment.ReadNoise(AnalogPin.P2))
     OLED.newLine()
     OLED.writeString("Temperature (c):")
     OLED.writeNum(Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C))
